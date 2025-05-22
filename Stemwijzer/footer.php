b98a60bca470eed27950ce78f31f01d229a71b52
David-264 <ps264577@summacollege.nl>
2025-05-22 11:50:14 +0200

    Stemwijzer site

---
+++ Stemwijzer/footer.php
@@ -0,0 +1,17 @@
+<html>
+    <head>
+        <title>footer</title>
+        <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+<?php
+
+?>
+
+<div id="footer-block">
+
+</div>
+
+</body>
+</html>
---
+++ Stemwijzer/index.php
@@ -0,0 +1,23 @@
+<html>
+    <head>
+        <title>index</title>
+        <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+<header>
+<?php
+include 'header.php';
+?> </header>
+
+
+
+<footer>
+<?php
+include 'footer.php'
+
+?> </footer>
+
+</body>
+</html>
+

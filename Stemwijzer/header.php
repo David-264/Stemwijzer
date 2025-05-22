d2dd052fd2ad4ee36a6758438a8814d375079591
David-264 <ps264577@summacollege.nl>
2025-05-22 11:42:30 +0200

    Add files via upload

---
+++ Stemwijzer/header.php
@@ -0,0 +1,37 @@
+<html>
+    <head>
+        <title>header</title>
+        <link rel="stylesheet" href="style.css">
+        
+</head>
+<body>
+
+<?php
+
+?>
+ <div id="header-block">
+
+ <div id="deel1">
+    <img id="logo" src="Logos/Logos/logo-neutraal-kieslab-donkerblauw.svg"/>
+    <p id="header-naam">Neutraal KiesLab</p>
+ </div>
+
+ <div id="deel2">
+ <button id="gebruiker-btn" type="button">
+    <img id="gebruiker-icoon" src="images/user.png"/>
+
+    
+</button>
+
+    <button id="darkmode-btn" type="button" aria-label="Toggle dark mode"> 
+    <img id="darkmode-icoon" src="images/darkmode.png" alt="Dark mode toggle"/>
+</button>
+</div>
+
+</div>
+
+
+
+
+</body>
+</html>
---
+++ Stemwijzer/partijenpagina.php
@@ -0,0 +1,13 @@
+<html>
+    <head>
+        <title>partijen</title>
+        <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+<?php
+
+?>
+
+</body>
+</html>
